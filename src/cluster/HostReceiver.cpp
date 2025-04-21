aaf6d2c9f6e28910810f35a9543c24aa4b90560d
Juanz231 <jdlopezg1@eafit.edu.co>
2025-04-20 22:26:42 -0500

    feat: add time stamp to announce message

--- src/cluster/HostReceiver.cpp
+++ src/cluster/HostReceiver.cpp
@@ -1,43 +1,56 @@
 // src/discovery/HostReceiver.cpp
 #include "../../include/cluster/HostReceiver.hpp"
 
-void runHostListener(FailoverManager &manager, int port) {
-  std::thread([&manager, port]() {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-      perror("Socket error");
-      return;
-    }
 
-    sockaddr_in serverAddr{};
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(port);
-
-    bind(server_fd, (sockaddr *)&serverAddr, sizeof(serverAddr));
-    listen(server_fd, 10);
-    std::cout << "[HostReceiver] Esperando conexiones TCP en puerto " << port
-              << std::endl;
-
-    while (true) {
-      sockaddr_in clientAddr{};
-      socklen_t addrLen = sizeof(clientAddr);
-      int client_fd = accept(server_fd, (sockaddr *)&clientAddr, &addrLen);
-
-      if (client_fd >= 0) {
-        char buffer[1024] = {0};
-        int len = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-        buffer[len] = '\0';
-
-        std::string newNode = std::string(buffer);
-        std::cout << "[HostReceiver] Nodo nuevo recibido: " << newNode
-                  << std::endl;
-
-        manager.addPeerAddress(newNode + ":50051"); // o el puerto gRPC usado
-        close(client_fd);
+  void runHostListener(FailoverManager & manager, int port) {
+    std::thread([&manager, port]() {
+      int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+      if (server_fd < 0) {
+        perror("Socket error");
+        return;
       }
-    }
 
-    close(server_fd);
-  }).detach();
-}
+      sockaddr_in serverAddr{};
+      serverAddr.sin_family = AF_INET;
+      serverAddr.sin_addr.s_addr = INADDR_ANY;
+      serverAddr.sin_port = htons(port);
+
+      bind(server_fd, (sockaddr *)&serverAddr, sizeof(serverAddr));
+      listen(server_fd, 10);
+      std::cout << "[HostReceiver] Esperando conexiones TCP en puerto " << port
+                << std::endl;
+
+      while (true) {
+        sockaddr_in clientAddr{};
+        socklen_t addrLen = sizeof(clientAddr);
+        int client_fd = accept(server_fd, (sockaddr *)&clientAddr, &addrLen);
+
+        if (client_fd >= 0) {
+          char buffer[1024] = {0};
+          int len = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+          buffer[len] = '\0';
+
+          std::string data(buffer);
+          auto sepPos = data.find('|');
+
+          if (sepPos != std::string::npos) {
+            std::string address = data.substr(0, sepPos);
+            std::string timestamp = data.substr(sepPos + 1);
+
+            std::cout << "[HostReceiver] Nodo recibido: " << address
+                      << " (Creado en: " << timestamp << ")" << std::endl;
+
+            manager.addPeerAddress(address);
+          } else {
+            std::cerr << "[HostReceiver] Formato inválido recibido: " << data
+                      << std::endl;
+          }
+
+          close(client_fd);
+        }
+      }
+
+      close(server_fd);
+    }).detach();
+  }
+
